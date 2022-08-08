92de0a3dd545bfcfad0e8d792fc70c82668d5107
Brandon <brandonrjguthrie@gmail.com>
2022-08-08 16:05:10 -0700

    create classed columns, prepare box and row arrays

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Assign value to grid div
+let esGrid = document.querySelector(".esGrid");
+
+//Create column div
+let gridColumn = document.createElement("div");
+
+//add column class to column
+gridColumn.classList.add("column");
+
+
+//declare empty array of rows and boxes to add divs and styles to
+let gridRows = [];
+let gridBoxes = [];
