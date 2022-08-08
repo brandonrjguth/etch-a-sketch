9a2a038e733dbd4efb34405cedbc05a7c4dfeea8
Brandon <brandonrjguthrie@gmail.com>
2022-08-08 16:06:14 -0700

    use loop to create classed rows and append to column

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ gridColumn.classList.add("column");
 //declare empty array of rows and boxes to add divs and styles to
 let gridRows = [];
 let gridBoxes = [];
+
+
+//Run a loop 8 times to create new row elements in an array called gridRow
+for (row = 0; row < 8; row++){
+
+    //Add each row to the gridRows array
+    gridRows[row] = document.createElement("div"); 
+
+    //add the 'row' class to each row
+    gridRows[row].classList.add("row");
+
+    //append each row to the grid column
+    gridColumn.appendChild(gridRows[row]);
+    
+}
