3d773a75e878b4443cca3a9cbc23fc67cd0e24ad
Brandon <brandonrjguthrie@gmail.com>
2022-08-08 17:08:40 -0700

    turn logic for creating grid into function

--- js/main.js
+++ js/main.js
@@ -13,35 +13,43 @@ let gridRows = [];
 let gridBoxes = [];
 
 
-//Run a loop 8 times to create new row elements in an array called gridRow
-for (row = 0; row < 8; row++){
 
-    //Add each row to the gridRows array
-    gridRows[row] = document.createElement("div"); 
+let createRow = () => {
 
-    //add the 'row' class to each row
-    gridRows[row].classList.add("row");
+    //Run a loop 8 times to create new row elements in an array called gridRow
+    for (row = 0; row < 8; row++){
 
-    //append each row to the grid column
-    gridColumn.appendChild(gridRows[row]);
+        //Add each row to the gridRows array
+        gridRows[row] = document.createElement("div"); 
+
+        //add the 'row' class to each row
+        gridRows[row].classList.add("row");
+
+        //append each row to the grid column
+        gridColumn.appendChild(gridRows[row]);
 
 
-    //For each of the 8 times a row is created and added to the array, create 8 boxes
-    for(box = 0; box < 8; box++){
+        //For each of the 8 times a row is created and added to the array, create 8 boxes
+        for(box = 0; box < 8; box++){
 
-        //create an array of 8 boxes for each of the 8 rows
-        gridBoxes[row*box] = document.createElement("div");
+            //create an array of 8 boxes for each of the 8 rows
+            gridBoxes[row*box] = document.createElement("div");
 
-        //add the box class to each of 64 boxes  
-        gridBoxes[row*box].classList.add("box");
+            //add the box class to each of 64 boxes  
+            gridBoxes[row*box].classList.add("box");
 
-        //add eventListener to each box, add "red" class to change color on mouseover
-        gridBoxes[row*box].addEventListener("mouseover", function(){this.classList.add("red")});
+            //add eventListener to each box, add "red" class to change color on mouseover
+            gridBoxes[row*box].addEventListener("mouseover", function(){this.classList.add("red")});
 
-        //append 8 boxes to each of the 8 rows
-        gridRows[row].appendChild(gridBoxes[row*box]);
+            //append 8 boxes to each of the 8 rows
+            gridRows[row].appendChild(gridBoxes[row*box]);
+        }
+
+
+    esGrid.appendChild(gridColumn);
     }
 
-
-esGrid.appendChild(gridColumn);
 }
+
+createRow();
+
